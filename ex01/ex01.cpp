d46736cfc205b368e1276ea9d9a0b13f498c980b
LiLangning <1900015417@pku.edu.cn>
2021-09-26 00:20:49 +0800

    test

--- ex01/ex01.cpp
+++ ex01/ex01.cpp
@@ -4,4 +4,7 @@ extern int ex0101();
 int main()
 {
 	ex0101();
+	// 修改一下看看
+	cout << "1"  ;
+	// test
 }
--- ex01/ex0101.cpp
+++ ex01/ex0101.cpp
@@ -2,6 +2,7 @@
 
 int ex0101()
 {
+	// a add function
 	int x, y;
 	scanf_s("%d %d", &x, &y);
 	int z = 0;
